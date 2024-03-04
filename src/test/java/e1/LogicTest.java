3ebd82858c2613c7ab32b287fdad4b64a72d677a
Corrado Stortini <Corrado.Stortini2@studio.unibo.it>
2024-03-04 11:18:28 +0100

    Create initial tests for the logic

--- src/test/java/e1/LogicTest.java
+++ src/test/java/e1/LogicTest.java
@@ -1,15 +1,51 @@
 package e1;
+
 import org.junit.jupiter.api.*;
 import static org.junit.jupiter.api.Assertions.*;
+
 public class LogicTest {
 
+  private static final int NEGATIVE_POSITION = -1;
+
+  private static final int SIZE = 10;
+  
+  private Logics logic;
+  private Pair<Integer, Integer> knight = new Pair<Integer, Integer>(1, 1);
+  private Pair<Integer, Integer> pawn = new Pair<Integer, Integer>(5, 5);
+
+  @BeforeEach
+  public void createLogic() {
+    logic = new LogicsImpl(SIZE, knight, pawn);
+  }
+
   @Test
-  public void test() {
-    assert(true);
-    // TODO: Add your test logic here
-    // You can generate random inputs and assert the expected output
-    // For example:
-    // int result = Logic.someMethod(5, 10);
-    // assertEquals(expectedResult, result);
+  public void testKnightIsNotOnNegativeAxis() {
+    assertAll(
+      () -> assertFalse(logic.hasKnight(NEGATIVE_POSITION, knight.getY())),
+      () -> assertFalse(logic.hasKnight(knight.getX(), NEGATIVE_POSITION))
+    );
+  }
+
+  @Test
+  public void testPawnIsNotOnNegativeAxis() {
+    assertAll(
+      () -> assertFalse(logic.hasPawn(NEGATIVE_POSITION, pawn.getY())),
+      () -> assertFalse(logic.hasPawn(pawn.getX(), NEGATIVE_POSITION))
+    );
+  }
+
+  @Test
+  public void testOutOfBoundHit() {
+    assertAll(
+      () -> assertThrows(IndexOutOfBoundsException.class, () -> logic.hit(1, NEGATIVE_POSITION)),
+      () -> assertThrows(IndexOutOfBoundsException.class, () -> logic.hit(NEGATIVE_POSITION, 1)),
+      () -> assertThrows(IndexOutOfBoundsException.class, () -> logic.hit(1, SIZE)),
+      () -> assertThrows(IndexOutOfBoundsException.class, () -> logic.hit(SIZE, 1))
+    );
+  }
+
+  @Test
+  public void testKnightMoveCorrectly() {
+    
   }
 }
