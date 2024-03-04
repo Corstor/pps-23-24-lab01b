8bb21a9cfc576e2c54d7ae1a3513b2d0315532a9
Corrado Stortini <Corrado.Stortini2@studio.unibo.it>
2024-03-04 12:17:42 +0100

    Test pawn and knight starting position (if they are in the right position and if they are not in the same position)

--- src/test/java/e1/LogicTest.java
+++ src/test/java/e1/LogicTest.java
@@ -3,12 +3,16 @@ package e1;
 import org.junit.jupiter.api.*;
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.function.BiFunction;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
 public class LogicTest {
 
   private static final int NEGATIVE_POSITION = -1;
 
   private static final int SIZE = 10;
-  
+
   private Logics logic;
   private Pair<Integer, Integer> knight = new Pair<Integer, Integer>(1, 1);
   private Pair<Integer, Integer> pawn = new Pair<Integer, Integer>(5, 5);
@@ -21,31 +25,59 @@ public class LogicTest {
   @Test
   public void testKnightIsNotOnNegativeAxis() {
     assertAll(
-      () -> assertFalse(logic.hasKnight(NEGATIVE_POSITION, knight.getY())),
-      () -> assertFalse(logic.hasKnight(knight.getX(), NEGATIVE_POSITION))
-    );
+        () -> assertFalse(logic.hasKnight(NEGATIVE_POSITION, knight.getY())),
+        () -> assertFalse(logic.hasKnight(knight.getX(), NEGATIVE_POSITION)));
   }
 
   @Test
   public void testPawnIsNotOnNegativeAxis() {
     assertAll(
-      () -> assertFalse(logic.hasPawn(NEGATIVE_POSITION, pawn.getY())),
-      () -> assertFalse(logic.hasPawn(pawn.getX(), NEGATIVE_POSITION))
-    );
+        () -> assertFalse(logic.hasPawn(NEGATIVE_POSITION, pawn.getY())),
+        () -> assertFalse(logic.hasPawn(pawn.getX(), NEGATIVE_POSITION)));
   }
 
   @Test
   public void testOutOfBoundHit() {
     assertAll(
-      () -> assertThrows(IndexOutOfBoundsException.class, () -> logic.hit(1, NEGATIVE_POSITION)),
-      () -> assertThrows(IndexOutOfBoundsException.class, () -> logic.hit(NEGATIVE_POSITION, 1)),
-      () -> assertThrows(IndexOutOfBoundsException.class, () -> logic.hit(1, SIZE)),
-      () -> assertThrows(IndexOutOfBoundsException.class, () -> logic.hit(SIZE, 1))
-    );
+        () -> assertThrows(IndexOutOfBoundsException.class, () -> logic.hit(1, NEGATIVE_POSITION)),
+        () -> assertThrows(IndexOutOfBoundsException.class, () -> logic.hit(NEGATIVE_POSITION, 1)),
+        () -> assertThrows(IndexOutOfBoundsException.class, () -> logic.hit(1, SIZE)),
+        () -> assertThrows(IndexOutOfBoundsException.class, () -> logic.hit(SIZE, 1)));
   }
 
   @Test
-  public void testKnightMoveCorrectly() {
-    
+  public void testKnightIsOnTheRightPosition() {
+    getPairIsPresent(this.knight, (x, y) -> this.logic.hasKnight(x, y));
+  }
+
+  @Test
+  public void testPawnIsOnTheRightPosition() {
+    getPairIsPresent(this.pawn, (x, y) -> this.logic.hasPawn(x, y));
+  }
+
+  private void getPairIsPresent(Pair<Integer, Integer> pairToFind, BiFunction<Integer, Integer, Boolean> filter) {
+    boolean isPresent = false;
+
+    for (int i = 0; i < SIZE; i++) {
+      for (int j = 0; j < SIZE; j++) {
+        if (filter.apply(i, j)) {
+          if (pairToFind.getX().equals(i) && pairToFind.getY().equals(j)) {
+            isPresent = true;
+          } else {
+            fail();
+          }
+        }
+      }
+    }
+
+    assertTrue(isPresent);
+  }
+
+  @Test
+  public void testKnightAndPawnOnSamePosition() {
+    assertAll(
+      () -> assertFalse(logic.hasKnight(this.pawn.getX(), this.pawn.getY())),
+      () -> assertFalse(logic.hasPawn(this.knight.getX(), this.knight.getY()))
+    );
   }
 }
