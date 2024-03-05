b1b9fdb4b5c8e5ac24e0e9cdcf1dac83d6c60b3c
Corrado Stortini <corrado.stortini2@studio.unibo.it>
2024-03-05 16:10:02 +0100

    Leave testMovementOutOfBound

--- src/test/java/e1/MovementTest.java
+++ src/test/java/e1/MovementTest.java
@@ -7,8 +7,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class MovementTest {
-    private static final int SIZE = 10;
-    private static final int NEGATIVE_POSITION = -1;
 
     private Movement movement;
 
@@ -17,19 +15,8 @@ public class MovementTest {
         this.movement = new Movement() {
             @Override
             public void move(int x, int y) {
-                if (x < 0 || y < 0 || x >= SIZE || y >= SIZE) {
-                    throw new IndexOutOfBoundsException();
-                }
+                
             }
         };
-    }
-
-    @Test
-    public void testMoveOutOfBounds() {
-        assertAll(
-        () -> assertThrows(IndexOutOfBoundsException.class, () -> movement.move(1, NEGATIVE_POSITION)),
-        () -> assertThrows(IndexOutOfBoundsException.class, () -> movement.move(NEGATIVE_POSITION, 1)),
-        () -> assertThrows(IndexOutOfBoundsException.class, () -> movement.move(1, SIZE)),
-        () -> assertThrows(IndexOutOfBoundsException.class, () -> movement.move(SIZE, 1)));
     }
 }
